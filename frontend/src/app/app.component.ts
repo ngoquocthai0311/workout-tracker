ab86baa0c5ed096da7aa774bb54c0f8a31f8a623
Thai.qng <ngoquocthai0311@gmail.com>
2025-09-16 22:31:54 +0700

    chore: testing mobile implementation on dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { Toast } from 'primeng/toast';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-root',
-  imports: [Toast, RouterModule, RouterOutlet, ScrollPanelModule],
+  imports: [
+    DrawerModule,
+    Toast,
+    RouterModule,
+    RouterOutlet,
+    ScrollPanelModule,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
+
+  visibleSideBar: boolean = false;
 
   ngOnInit() {}
 }
