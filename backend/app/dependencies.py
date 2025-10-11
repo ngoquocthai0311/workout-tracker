33374496f77ce23cc7886fb5ebc1e6cfe0ce3e92
Quốc Thái <50079483+ngoquocthai0311@users.noreply.github.com>
2025-10-11 22:05:35 +0700

    feat: set up redis and cache exercises (#5)

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,3 +1,4 @@
+from fastapi import Depends
 from sqlmodel import Session, create_engine
 from app.routers.mappers.sessions_mapper import WorkoutSessionMapper
 from functools import lru_cache
@@ -8,6 +9,10 @@ from dotenv import load_dotenv
 import os
 from os.path import join
 from pathlib import Path
+import redis
+from redis import Redis
+import json
+from typing import Optional
 
 
 class Settings:
@@ -23,14 +28,64 @@ class Settings:
         # getenv for unnecessary env vars
         self.DATABASE_ECHO: bool = bool(os.getenv("DATABASE_ECHO", 0))
 
+        self.REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
+        self.REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
+        self.REDIS_DATABASE_INDEX: int = int(os.getenv("REDIS_DATABASE_INDEX", 0))
+
         self.DATABASE_URL: str = f"postgresql+psycopg2://{self.DATABASE_USER}:{self.DATABASE_PASS}@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
         self.SQLALCHEMY_URL: str = f"postgresql://{self.DATABASE_USER}:{self.DATABASE_PASS}@{self.DATABASE_HOST}/{self.DATABASE_NAME}"
+
+
+class RedisService:
+    def __init__(self, redis_session: Optional[Redis]):
+        self.redis_session: Optional[Redis] = redis_session
+        # 2 hours
+        self.key_duration: int = 7200
+
+    def get_value(self, key: str):
+        if self.redis_session:
+            try:
+                value_str = self.redis_session.get(key)
+                if value_str:
+                    return json.loads(value_str)
+            except Exception as e:
+                # NOTE: Do logging here
+                print("Redis may be failed or empty")
+                print(e)
+                pass
+
+        return None
+
+    def cache_value(self, key: str, value: list | object):
+        if self.redis_session:
+            # maybe trigger background tasks here instead of waiting
+            # save response in 2 hours
+            try:
+                self.redis_session.setex(
+                    key,
+                    self.key_duration,
+                    json.dumps([item.__dict__ for item in value])
+                    if isinstance(value, list)
+                    else json.dumps(value),
+                )
+            except Exception as e:
+                print("Can't cache_value")
+                print(e)
+
+    def remove_cache(self, key: str):
+        if self.redis_session:
+            try:
+                self.redis_session.delete(key)
+            except Exception as e:
+                print("Can't remove cache")
+                print(e)
 
 
 # use this to init database
 # SQLModel.metadata.create_all(engine)
 
 
+# NOTE: caching Settings object instance
 @lru_cache
 def get_settings():
     return Settings()
@@ -39,7 +94,8 @@ def get_settings():
 engine = create_engine(get_settings().DATABASE_URL, echo=get_settings().DATABASE_ECHO)
 
 
-# @lru_cache
+# NOTE: decorator function to trigger guarantee clean up after use
+# TODO: create a connection pool and let other uses it
 # @contextmanager
 def get_db():
     with Session(engine) as session:
@@ -64,3 +120,24 @@ def get_report_mapper():
 @lru_cache
 def get_exercise_mapper():
     return ExerciseMapper()
+
+
+# TODO: create a connection pool and let other uses it
+# @contextmanager
+def get_redis():
+    try:
+        with redis.Redis(
+            host=os.getenv("REDIS_HOST", "localhost"),
+            port=os.getenv("REDIS_PORT", 6379),
+            db=os.getenv("REDIS_DATABASE_INDEX", 0),
+            decode_responses=True,
+        ) as session:
+            yield session
+    except Exception as e:
+        print("Cant connect to redis database")
+        print(e)
+        yield None
+
+
+def get_redis_service(redis_session=Depends(get_redis)):
+    return RedisService(redis_session)
