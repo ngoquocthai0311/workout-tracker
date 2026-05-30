208b8e0e20da844b15d6440a5349f42d9074ad2a
Quoc Thai <ngoquocthai0311@gmail.com>
2026-05-30 21:45:40 +0700

    chore: update dependencies

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,11 +1,5 @@
-from app.database.dashboard_repository import DashboardRepository
-from app.database.routine_repository import RoutineRepository
-from app.database.session_repository import SessionRepository
 from sqlmodel import Session, create_engine
-from app.routers.mappers.sessions_mapper import WorkoutSessionMapper
 from functools import lru_cache
-from app.routers.mappers.routines_mapper import RoutineMapper
-from app.routers.mappers.dashboard_mapper import ReportMapper
 from dotenv import load_dotenv
 import os
 from os.path import join
@@ -57,78 +51,3 @@ def get_db():
         raise e
     finally:
         session.close()
-
-
-@lru_cache
-def get_workout_session_mapper():
-    return WorkoutSessionMapper()
-
-
-@lru_cache
-def get_routine_mapper():
-    return RoutineMapper()
-
-
-@lru_cache
-def get_report_mapper():
-    return ReportMapper()
-
-
-@lru_cache
-<<<<<<< HEAD
-def get_exercise_mapper():
-    return ExerciseMapper()
-
-
-# TODO: create a connection pool and let other uses it
-# @contextmanager
-def get_redis():
-    session = None
-    try:
-        redis_url = os.getenv("REDIS_URL")
-        redis_host = os.getenv("REDIS_HOST", "localhost")
-        if redis_url:
-            session = redis.Redis.from_url(redis_url, decode_responses=True)
-        elif redis_host.startswith("redis://") or redis_host.startswith("rediss://"):
-            session = redis.Redis.from_url(redis_host, decode_responses=True)
-        else:
-            session = redis.Redis(
-                host=redis_host,
-                port=int(os.getenv("REDIS_PORT", 6379)),
-                db=int(os.getenv("REDIS_DATABASE_INDEX", 0)),
-                decode_responses=True,
-            )
-        yield session
-    except Exception as e:
-        print("Cant connect to redis database")
-        print(e)
-        raise e
-    finally:
-        if session:
-            session.close()
-
-
-def get_redis_service(redis_session=Depends(get_redis)):
-    return RedisService(redis_session)
-
-
-@lru_cache
-def get_exercise_repository():
-    return ExerciseRepository()
-
-
-@lru_cache
-=======
->>>>>>> 5e3a0e7 (feat: rework backend code)
-def get_routine_repository():
-    return RoutineRepository()
-
-
-@lru_cache
-def get_session_repository():
-    return SessionRepository()
-
-
-@lru_cache
-def get_dashboard_repository():
-    return DashboardRepository()
